8c7f1731bb41ee3854a9fe419228def38047967e
Sala 2 <vanegasivanc@gmail.comgit config user.email vanegasivanc@gmail.com>
2025-12-05 08:22:23 -0500

    Añadí algunos models bro, voy a intentar terminarlos

--- models.py
+++ models.py
@@ -1,6 +1,20 @@
-class Jugador():
-    pass
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List
 
+class JugadorBase(SQLModel):
+    nombre: Optional[str]=None
+    altura: Optional[str]=None
+    peso: Optional[str]=None
+    pie_dominante: Optional[str]=None
+    tiempo_cancha: Optional[str]=None
+    goles: Optional[str]=None
+    faltas: Optional[str]=None
+    
+
+class Jugador(JugadorBase, table=True):
+    dorsal:Optional[str]=None
+    active: bool = Field(default=True)
+    estadisticas:
 
 class Estadistica():
     pass
@@ -8,5 +22,4 @@ class Estadistica():
 
 class Partido():
     pass
-
 
